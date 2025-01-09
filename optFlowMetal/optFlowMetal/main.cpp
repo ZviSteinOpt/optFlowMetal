a5524ba7f4390815e1d3e5e80c42db30a6c5957b
Zvi <96817396+ZviSteinOpt@users.noreply.github.com>
2025-01-09 22:04:29 +0200

    Initial commit

---
+++ optFlowMetal/optFlowMetal/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  optFlowMetal
+//
+//  Created by ZVI STEIN on 09/01/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
